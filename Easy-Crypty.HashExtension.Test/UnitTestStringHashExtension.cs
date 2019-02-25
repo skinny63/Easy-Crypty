0f4e8057c06afba4fd1c780daca8d05e5915d4f7
Valentin <valentin.damiens63@gmail.com>
2019-02-25 20:33:31 +0100

    Implement Md5 hash test

---
+++ Easy-Crypty.HashExtension.Test/UnitTestStringHashExtension.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Easy_Crypty.HashExtension.Test
+{
+    public class UnitTestStringHashExtension
+    {
+        /// <summary>
+        /// test Md5 hash
+        /// </summary>
+        [Fact]
+        public void TestMethodMd5Hash() => Assert.Equal("f9d08276bc85d30d578e8883f3c7e843", "md5hash".Md5Hash());
+        
+    }
+}
